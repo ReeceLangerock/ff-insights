4e7cb841b40de51dc7d70d62f117df65c0869fe3
reecelangerock <reecelangerock@gmail.com>
2019-08-11 20:42:22 -0500

    add .env var for axios

--- src/lib/AxiosHelper.js
+++ src/lib/AxiosHelper.js
@@ -1,7 +1,12 @@
 import axios from "axios"
+const ROOT_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "http://insights-env.jse23gjs93.us-east-2.elasticbeanstalk.com"
+
 export const getLeagueList = async () => {
   try {
-    const response = await axios.get("http://localhost:3001/leagues")
+    const response = await axios.get(`${ROOT_URL}/leagues`)
     return response
   } catch (error) {
     console.error(error)
@@ -10,18 +15,16 @@ export const getLeagueList = async () => {
 
 export const getInsight = async (id, matchup) => {
   try {
-    const response = await axios.get(
-      `http://localhost:3001/insights/${id}/${matchup}`
-    )
+    const response = await axios.get(`${ROOT_URL}/insights/${id}/${matchup}`)
     return response.data.insight
   } catch (error) {
     console.error(error)
   }
 }
 
-export const getInsights = async (id) => {
+export const getInsights = async id => {
   try {
-    const response = await axios.get(`http://localhost:3001/insights/${id}`)
+    const response = await axios.get(`${ROOT_URL}/insights/${id}`)
     return response.data.insights
   } catch (error) {
     console.error(error)
--- src/pages/league.js
+++ src/pages/league.js
@@ -3,17 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import League from "../components/League";
+import League from "../components/League"
 
-const Leagues = (props) => 
-{
-    return(
-    
-  <Layout>
-    <SEO title="LeagueList" />
-    <League path = {props}/>
-   
-  </Layout>
-)}
+const Leagues = props => {
+  return (
+    <Layout>
+      <SEO title="LeagueList" />
+      <League path={props} />
+    </Layout>
+  )
+}
 
 export default Leagues
