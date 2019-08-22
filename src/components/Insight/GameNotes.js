c6809a1109f66ed45c7abdffd052b6a174147851
reecelangerock <reecelangerock@gmail.com>
2019-08-21 21:08:07 -0500

    fix ordinal

--- src/components/Insight/GameNotes.js
+++ src/components/Insight/GameNotes.js
@@ -1,27 +1,30 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React, { Component } from "react"
+import styled from "styled-components"
+import NLP from "./../../lib/NLP"
 
 export default class GameNotes extends Component {
   render() {
-    const { data } = this.props;
-    if(!data){
-      return <Container/>
+    const { data } = this.props
+    if (!data) {
+      return <Container />
     }
     return (
       <Container>
         <h2>Game Notes</h2>
         <Text>
-          The {data.gameNotes.margin} point margin of victory for this matchup ended up being the{" "}
-          {data.gameNotes.marginOfVictoryRank}th largest in the league this week.
+          The {data.gameNotes.margin} point margin of victory for this matchup
+          ended up being the{" "}
+          {NLP.highLowHelper(data.gameNotes.marginOfVictoryRank)} in the league
+          this week.
         </Text>
       </Container>
-    );
+    )
   }
 }
 const Container = styled.div`
   padding: 1.5rem 1rem;
   margin: 0;
-`;
+`
 const Text = styled.p`
   padding-left: 1rem;
-`;
+`
