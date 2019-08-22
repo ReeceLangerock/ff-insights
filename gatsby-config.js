099959d783e8fce800a964b5b23c107390ba1043
reecelangerock <reecelangerock@gmail.com>
2019-08-22 06:42:37 -0500

    add styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@reecelangerock`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
