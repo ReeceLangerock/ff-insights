644d8146000a18b02235b83112347260b5405b48
reecelangerock <reecelangerock@gmail.com>
2019-08-25 14:11:50 -0500

    add toast, and some form cleanup

---
+++ src/components/General/Toast.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react"
+import { styled as MUIStyled } from "@material-ui/styles"
+import { Snackbar, SnackbarContent } from "@material-ui/core"
+import { Warning, Close, Info, Error, CheckCircle } from "@material-ui/icons"
+
+export default class Toast extends Component {
+  render() {
+    const { responseType } = this.props
+    const bgColor = responseType === "success" ? "#43a047" : "#d32f2f"
+    return (
+      <StyledSnackbar
+        className="error"
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={this.props.open}
+        autoHideDuration={50000}
+        onClose={this.props.handleClose}
+        ContentProps={{
+          "aria-describedby": "message-id",
+        }}
+      >
+        <SnackbarContent
+          style={{
+            background: bgColor,
+          }}
+          id="message-id"
+          message={
+            <span
+              id="client-snackbar"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              {this.props.responseType === "success" ? (
+                <CheckCircle style={{ marginRight: ".5rem" }} />
+              ) : (
+                <Error style={{ marginRight: ".5rem" }} />
+              )}
+              {this.props.message}
+            </span>
+          }
+        ></SnackbarContent>
+      </StyledSnackbar>
+    )
+  }
+}
+
+const StyledSnackbar = MUIStyled(Snackbar)({})
--- src/components/Signup/Form.js
+++ src/components/Signup/Form.js
@@ -3,7 +3,10 @@ import styled from "styled-components"
 import { styled as MUIStyled } from "@material-ui/styles"
 import { addLeague } from "./../../lib/AxiosHelper"
 import FormItem from "./FormItem"
-import Button from "@material-ui/core/Button"
+import { Button, Card } from "@material-ui/core"
+import Toast from "../General/Toast"
+import Instructions from "./Instructions"
+import { leagueId, email, isPrivate, SWID, ESPN_S2 } from "./formData"
 
 export default class Form extends Component {
   constructor(props) {
@@ -46,12 +49,10 @@ export default class Form extends Component {
   render() {
     const { form } = this.state
     return (
-      <StyledForm onSubmit={e => this.handleSubmit(e)}>
-        <Fieldset>
+      <div>
+        <FormGroup>
           <FormItem
-            label="League Id"
-            infoText="The Id for your league can be found in the ESPN website url when you are on your leagues page."
-            infoImageSrc="https://d16nyafvsvwqce.cloudfront.net/leagueIdExample.png"
+            data={leagueId}
             type="text"
             value={form.leagueId}
             pattern=".{0}|.{4,}"
@@ -60,8 +61,7 @@ export default class Form extends Component {
           ></FormItem>
 
           <FormItem
-            label="Your email address"
-            infoText="Why do we need you email? We will send you a weekly email when new insights are available for your league. We may also send a short email in rare cases to inform you about major changes or problems with the site."
+            data={email}
             type="email"
             value={form.email}
             pattern=".{0}|.{8,}"
@@ -70,36 +70,18 @@ export default class Form extends Component {
           ></FormItem>
 
           <FormItem
-            label="League is private"
+           data = {isPrivate}
             type="checkbox"
             value={form.isPrivate}
             onChange={e => this.handleChange("isPrivate", !form.isPrivate)}
           ></FormItem>
-        </Fieldset>
+        </FormGroup>
 
-        <Fieldset hidden={form.isPrivate === false}>
-          <p>
-            Unfortunately, for a private league you'll need to do a little bit
-            of extra work in the developer panel of your web browser. I'll add a
-            detailed walkthrough of this in the future.
-          </p>
-          <ol>
-            <li>Open up developer panel. </li>
-            <li>
-              <b>Chrome / Brave: </b>Go to the application tab and open cookies
-              on the left.
-              <br />
-              <b>Firefox / Safari: </b>Go to the storage tab and open cookies on
-              the left
-            </li>
-            <li>
-              Select the cookie for fantasy.espn and then search for the entry
-              for swid and espn_s2.
-            </li>
-          </ol>
+        <FormGroup hidden={form.isPrivate === false}>
+          <Instructions />
 
           <FormItem
-            label="SWID"
+           data={SWID}
             type="text"
             value={form.SWID}
             required={form.isPrivate}
@@ -108,38 +90,37 @@ export default class Form extends Component {
           ></FormItem>
 
           <FormItem
-            label="ESPN_S2"
+            data={ESPN_S2}
             type="text"
             value={form.ESPN_S2}
             required={form.isPrivate}
             pattern=".{0}|.{8,}"
             onChange={e => this.handleChange("ESPN_S2", e.target.value)}
           ></FormItem>
-        </Fieldset>
+        </FormGroup>
 
-        {this.state.showToast && <div>{this.state.submissionMessage}</div>}
-        <StyledButton variant="contained">Sign Up</StyledButton>
-      </StyledForm>
+        <Toast
+          open={this.state.showToast}
+          handleClose={() => this.setState({ showToast: false })}
+          message={this.state.submissionMessage}
+          responseType={this.state.responseType}
+        ></Toast>
+        <StyledButton
+          variant="contained"
+          onClick={this.handleSubmit.bind(this)}
+        >
+          Sign Up
+        </StyledButton>
+      </div>
     )
   }
 }
-
-const Fieldset = styled.fieldset`
-  display: ${props => (props.hidden ? "none" : "block")};
-  margin: 0 0 1rem 0;
-`
-
 const StyledButton = MUIStyled(Button)({
   marginLeft: "auto",
 })
-//   background: #c83802;
-//   color: #fff;
-//   padding: 0.6rem 1.5rem;
-//   border-radius: var(--border-radius-s);
-//   outline: none;
-//   border: 1px solid hsla(17, 100%, 7%, 1);
-//   font-size: 1rem;
-//   cursor: pointer;
-// `
 
-const StyledForm = styled.form``
+const FormGroup = MUIStyled(Card)({
+  display: `${props => (props.hidden ? "none" : "block")}`,
+  padding: "1rem",
+  marginBottom: "1rem",
+})
