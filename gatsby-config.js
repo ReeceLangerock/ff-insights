782e53a9a70fe38612aa1ea42c3e784a53cbdf58
reecelangerock <reecelangerock@gmail.com>
2019-08-25 14:23:13 -0500

    fix metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Insightful`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Take your ESPN fantasy football league to the next level with automatically generated matchup analysis and breakdowns`,
     author: `@reecelangerock`,
   },
   plugins: [
@@ -23,8 +23,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#c83802`,
+        theme_color: `#c83802`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
