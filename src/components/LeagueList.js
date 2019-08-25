fbe4dc07850add3adfec17c9bbd3d45a665440e5
reecelangerock <reecelangerock@gmail.com>
2019-08-25 09:04:02 -0500

    styling fix, add leagues to redux

--- src/components/LeagueList.js
+++ src/components/LeagueList.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import { getLeagueList } from "./../lib/AxiosHelper"
-
+import { styled as MUIStyled } from "@material-ui/styles"
+import { Card, Button } from "@material-ui/core"
 import styled from "styled-components"
+import { connect } from "react-redux"
+import * as actions from "./../redux/actions/leagueActions"
 
-export default class LeagueList extends Component {
+class LeagueList extends Component {
   state = {
     leagues: [],
   }
   async componentDidMount() {
-    const response = await getLeagueList()
-    this.setState({ leagues: response.leagues })
+    if (!this.props.leagues.length) {
+      const response = await getLeagueList()
+      this.props.saveLeagues(response.leagues)
+    }
   }
   renderLeagues() {
-    return this.state.leagues.map(league => {
+    return (this.props.leagues || []).map(league => {
       return (
-        <LeagueItem to={`/league/?id=${league.leagueId}`} key={league.leagueId}>
-          <div>{league.leagueName}</div>
-          <div>{league.leagueId}</div>
+        <LeagueItem key={league.leagueId}>
+          {league.logo && <img></img>}
+          <div>
+            <h4>League Name</h4>
+            <span>{league.leagueName}</span>
+          </div>
+          <div>
+            <h4>League Id</h4>
+            <span>{league.leagueId}</span>
+          </div>
+
+          <StyledLink to={`/league/?id=${league.leagueId}`}>
+            <Button variant="contained" color="primary">
+              Go to league
+            </Button>
+          </StyledLink>
         </LeagueItem>
       )
     })
@@ -33,11 +51,32 @@ export default class LeagueList extends Component {
   }
 }
 
-const LeagueItem = styled(Link)`
-  display: flex;
-  border: 1px solid gray;
-  padding: 1rem;
-  margin:  1rem 0;
+const mapStateToProps = state => ({
+  leagues: state.leagueReducer.leagues,
+})
+
+export default connect(
+  mapStateToProps,
+  actions
+)(LeagueList)
+
+const StyledLink = styled(Link)`
   text-decoration: none;
-  justify-content: space-between;
+  color: inherit;
 `
+
+const LeagueItem = MUIStyled(Card)({
+  padding: "2rem 1.5rem",
+  marginBottom: "1rem",
+  alignItems: "center",
+  display: "flex",
+  justifyContent: "space-between",
+  "& h4": {
+    margin: "0 0 .6rem 0",
+  },
+  "& div": {
+    flex: 1,
+
+    marginRight: "1rem",
+  },
+})
