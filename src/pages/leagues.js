d726149076bd011310ebc6eae0baa0bb87f00808
reecelangerock <reecelangerock@gmail.com>
2019-08-27 06:33:29 -0500

    fix component path

--- src/pages/leagues.js
+++ src/pages/leagues.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import LeagueList from "../components/LeagueList";
+import LeagueList from "../components/League/LeagueList";
 
 const Leagues = () => 
 {
