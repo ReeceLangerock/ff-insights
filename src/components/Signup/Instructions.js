2b86eae930f92096d359c6723e7fa90b1eb686c0
reecelangerock <reecelangerock@gmail.com>
2019-08-28 19:31:28 -0500

    add more to instrutcions

--- src/components/Signup/Instructions.js
+++ src/components/Signup/Instructions.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react"
 
 export default function Instructions() {
-    return (
-        <div>
-            <p>
-            Unfortunately, for a private league you'll need to do a little bit
-            of extra work in the developer panel of your web browser. I'll add a
-            detailed walkthrough of this in the future.
-          </p>
-          <ol>
-            <li>Open up developer panel. </li>
-            <li>
-              <b>Chrome / Brave: </b>Go to the application tab and open cookies
-              on the left.
-              <br />
-              <b>Firefox / Safari: </b>Go to the storage tab and open cookies on
-              the left
-            </li>
-            <li>
-              Select the cookie for fantasy.espn and then search for the entry
-              for swid and espn_s2.
-            </li>
-          </ol>
-        </div>
-    )
+  return (
+    <div>
+      <p>
+        Unfortunately, for a private league ESPN requires two pieces of data
+        that you'll need to do provide. It requires a little bit of work in the
+        developer panel of your web browser, but you'll only need to do it once!
+        I'll add a detailed walkthrough of this in the future.
+      </p>
+      <ol>
+        <li>Open up developer panel. </li>
+        <li>
+          <b>Chrome / Brave: </b>Go to the application tab and open cookies on
+          the left.
+          <br />
+          <b>Firefox / Safari: </b>Go to the storage tab and open cookies on the
+          left
+        </li>
+        <li>
+          Select the cookie for fantasy.espn and then search for the entry for
+          swid and espn_s2.
+        </li>
+      </ol>
+    </div>
+  )
 }
