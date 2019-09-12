28dad61a68e519421d50d65c32f1b772b87ea577
reecelangerock <reecelangerock@gmail.com>
2019-09-12 06:03:36 -0500

    fix what if text

--- src/lib/WhatIfHelper.js
+++ src/lib/WhatIfHelper.js
@@ -1,13 +1,23 @@
 export default {
   generateOtherTeamsBeatenText(teamName, opponentName, numBeaten) {
-    const texts = [
-      `Besides ${opponentName}, ${teamName} would have beaten ${numBeaten} other teams this week.`,
-      `In addition to ${opponentName}, ${teamName} would have triumphed over ${numBeaten} other teams this week.`,
-    ]
+    let texts
+    if (numBeaten === 1) {
+      texts = [
+        `${teamName} got lucky with their matchup, as they would have lost to every other team except for ${opponentName}`,
+      ]
+    } else {
+      texts = [
+        `Besides ${opponentName}, ${teamName} would have beaten ${numBeaten} other teams this week.`,
+        `In addition to ${opponentName}, ${teamName} would have triumphed over ${numBeaten} other teams this week.`,
+      ]
+    }
     const rand = Math.floor(Math.random() * texts.length)
     return texts[rand]
   },
   generateOtherTeamsLostToText(winningTeam, losingTeam, numLostTo) {
+    if(numLostTo === 0){
+      return ""
+    }
     const texts = [
       `Besides ${winningTeam}, ${losingTeam} would have lost to ${numLostTo} other teams this week.`,
       `In addition to ${winningTeam}, ${losingTeam} would have been defeated by ${numLostTo} other teams this week.`,
