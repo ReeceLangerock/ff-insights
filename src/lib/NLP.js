f20f2cf20187b18cfe30a146ebfb0bd5b332e5ca
reecelangerock <reecelangerock@gmail.com>
2019-09-14 15:38:46 -0500

    tweak highLowHelper

--- src/lib/NLP.js
+++ src/lib/NLP.js
@@ -1,16 +1,19 @@
 export default {
   getOrdinal(n) {
     var s = ["th", "st", "nd", "rd"],
-      v = n % 100;
-    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+      v = n % 100
+    return n + (s[(v - 20) % 10] || s[v] || s[0])
   },
   highLowHelper(rank, count) {
     if (rank === 1) {
-      return "highest";
+      return "highest"
     } else if (rank === count) {
-      return "lowest";
+      return "lowest"
+    } else if (count) {
+      const hl = rank < count / 2 ? "highest" : "lowest"
+      return `${this.getOrdinal(rank)} ${hl}`
     } else {
-      return `${this.getOrdinal(rank)} highest`;
+      return `${this.getOrdinal(rank)} highest`
     }
-  }
-};
+  },
+}
