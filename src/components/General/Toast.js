03cf22f026a4337d10fdabc7497a1c3c089abc69
reecelangerock <reecelangerock@gmail.com>
2019-09-14 19:53:30 -0500

    test

--- src/components/General/Toast.js
+++ src/components/General/Toast.js
@@ -39,6 +39,7 @@ export default class Toast extends Component {
               {this.props.message}
             </span>
           }
+          
         ></SnackbarContent>
       </StyledSnackbar>
     )
