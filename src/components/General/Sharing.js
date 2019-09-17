c756bad13f36fd073a11892cba6e5f7bac9002e5
reecelangerock <reecelangerock@gmail.com>
2019-09-16 19:40:29 -0500

    add twitter sharing

--- src/components/General/Sharing.js
+++ src/components/General/Sharing.js
@@ -7,6 +7,7 @@ export default class Sharing extends Component {
   state = {}
   componentDidMount() {
     this.createBBCode()
+    this.createTweetLink()
   }
 
   createBBCode() {
@@ -18,11 +19,21 @@ export default class Sharing extends Component {
         const url = `https://insightful.tk/insight/?id=${data.leagueId}&matchup=${insight.matchupId}`
         bbString += `[url=${url}]${insight.awayTeam.name} vs. ${insight.homeTeam.name}[/url]\n`
       })
-      console.log(bbString)
       this.setState({ bbcode: bbString })
     }
   }
+
+  createTweetLink() {
+    const { data } = this.props
+    const base = `https://twitter.com/intent/tweet?text=`
+    const text =
+      `Check out week ${data.week} insights for ${data.leagueName}, only on Insightful.` +
+      `\nhttps://insightful.tk/insight/?id=${data.leagueId}`
+    const link = encodeURI(base + text)
+    this.setState({ twitterLink: link })
+  }
   render() {
+    const { data } = this.props
     return (
       <>
         <h2 style={{ marginTop: "3rem" }}>Share your League Insights</h2>
@@ -35,7 +46,7 @@ export default class Sharing extends Component {
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                href={`https://fantasy.espn.com/football/league?leagueId=${this.props.data.leagueId}`}
+                href={`https://fantasy.espn.com/football/league?leagueId=${data.leagueId}`}
               >
                 league home page
               </a>
@@ -49,6 +60,23 @@ export default class Sharing extends Component {
             >
               Copy To Clipboard
             </Button>
+          </Share>
+          <Share>
+            <h3>Social Media</h3>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={this.state.twitterLink}
+            >
+              <Image
+                xmlns="http://www.w3.org/2000/svg"
+                width="45"
+                height="45"
+                viewBox="0 0 24 24"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+              </Image>
+            </a>
           </Share>
         </Container>
       </>
@@ -79,3 +107,9 @@ const Share = MUIStyled(Card)({
     fontWeight: "bold",
   },
 })
+
+const Image = styled.svg`
+  height: 45px;
+  fill: var(--primary-color);
+  margin-top: 1rem;
+`
