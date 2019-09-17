8bc88873036a3da52d6334043be529ba5a2a682f
reecelangerock <reecelangerock@gmail.com>
2019-09-16 21:19:26 -0500

    add gtag i guess

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+  
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-148167774-1",
+        // your google analytics tracking id
+        trackingId: `UA-148167774-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
     },
     `gatsby-transformer-sharp`,
