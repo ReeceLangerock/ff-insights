890dc99183e950bb7d511c9a808462c9e3e823b0
reecelangerock <reecelangerock@gmail.com>
2019-09-21 15:12:44 -0500

    add toast

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux"
 
 import { insightsReducer } from "./insightsReducer"
 import { leagueReducer } from "./leagueReducer"
+import { toastReducer } from "./toastReducer"
 
 export default combineReducers({
   insightsReducer,
   leagueReducer,
+  toastReducer,
 })
