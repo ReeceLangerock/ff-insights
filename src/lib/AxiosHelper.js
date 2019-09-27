66cba7c7fef1d3cae47d72b8f5fbfe8bf2698697
Reece Langerock <reecelangerock@gmail.com>
2019-09-27 10:17:03 -0500

    Update AxiosHelper.js

--- src/lib/AxiosHelper.js
+++ src/lib/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const ROOT_URL =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
     : "https://aw2ua68a2j.execute-api.us-east-2.amazonaws.com/production"
 
